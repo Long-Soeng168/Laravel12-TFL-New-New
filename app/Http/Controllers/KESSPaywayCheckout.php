1752284e06f2a5e8fb4b1656492da580ae39bc4e
Long-Soeng167 <generalpurpose167@gmail.com>
2025-11-20 15:58:29 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app/Http/Controllers/KESSPaywayCheckout.php

--- app/Http/Controllers/KESSPaywayCheckout.php
+++ app/Http/Controllers/KESSPaywayCheckout.php
@@ -161,10 +161,18 @@ class KESSPaywayCheckout extends Controller
     {
         $order = Order::where('tran_id', $request->tran_id)->first();
 
-        $order->update([
-            'transaction_detail' => $request->all(),
-            'notes' => 'requested',
-        ]);
+        if ($order) {
+            $order->update([
+                'transaction_detail' => $request->tran_id,
+                // 'notes' => 'requested',
+            ]);
+        } else {
+            $order->update([
+                'transaction_detail' => 'Failed',
+                // 'notes' => 'requested',
+            ]);
+        }
+
 
         if ($order->status == 'pending' && $order->payment_status != 'SUCCESS') {
             $merchant = new Merchants();
